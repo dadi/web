58c9120ea1f7fd1a7eca540092b61e6e4602ee07
James Lambie <jl@wearedaddy.com>
2015-05-27 08:58:15 +0000

    added usage info for new Dust helper

--- bantam/lib/dust/helpers.js
+++ bantam/lib/dust/helpers.js
@@ -1,6 +1,9 @@
 var dust = require('dustjs-linkedin');
-//var dustHelpers = require('dustjs-helpers');
 
+/*
+* Returns the supplied 'data' parameter truncated using the supplied 'length' parameter 
+* Usage: {@Truncate data="{body}" length="250"/}
+*/
 dust.helpers.Truncate = function(chunk, context, bodies, params) {
     var data   = dust.helpers.tap(params.data, chunk, context),
         length = dust.helpers.tap(params.length, chunk, context);
