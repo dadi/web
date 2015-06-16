6cef3469a71eacbb5679ee41c686e48a6cb4142d
Viktor Fero <feviktor@gmail.com>
2015-06-16 11:28:04 +0100

    Fix for page load issue if no ds attached
    Fix for issue when page fails to load if no data-source attached

--- bantam/lib/controller/index.js
+++ bantam/lib/controller/index.js
@@ -115,75 +115,78 @@ Controller.prototype.loadData = function(req, res, data, done) {
   delete query.debug;
 
   var path = url.parse(req.url).pathname.replace('/','');
-
-  _.each(self.datasources, function(value, key) {
-
-    var ds = self.datasources[key];
-
-    var filter = {};
-
-    if (key.indexOf(data.title) >= 0) {
-
-      ds.schema.datasource.page = query.page || 1;
-      delete query.page;
-      
-      filter = query;
-      
-      if (req.params.id || filter.id) {
-        filter._id = req.params.id || filter.id;
-        delete filter.id;
-      }
-
-      ds.schema.datasource.filter = {};
-
-      _.each(filter, function(value, key) {
-          ds.schema.datasource.filter[key] = value;
-      });
-
-      // rebuild datasource endpoint with filters
-      var d = new Datasource();
-      d.buildEndpoint(ds.schema, function(endpoint) {
-        ds.endpoint = endpoint;
-      });
-
-    }
-
-    help.getData(ds, function(result) {
-
-      if (result) {
-        data[key] = (typeof result === 'object' ? result : JSON.parse(result));
-      }
-
-      idx++;
-
-      // if we're at the end of the datasources array, 
-      // start processing the attached events
-      if (idx === Object.keys(self.datasources).length) {
-
-
-        if (0 !== Object.keys(self.events).length) {
-          var eventIdx = 0;
-          _.each(self.events, function(value, key) {
-            
-              self.events[key].run(req, res, function(result) {
-                data[key] = result;
-              });
-
-              eventIdx++;
-
-              // return the data if we're at the end of the events
-              // array, we have all the responses to render the page
-              if (eventIdx === Object.keys(self.events).length) {
-                done(data);
-              }
-          });
+  if (Array.isArray(self.datasources)) {
+    _.each(self.datasources, function(value, key) {
+  
+      var ds = self.datasources[key];
+  
+      var filter = {};
+  
+      if (key.indexOf(data.title) >= 0) {
+  
+        ds.schema.datasource.page = query.page || 1;
+        delete query.page;
+        
+        filter = query;
+        
+        if (req.params.id || filter.id) {
+          filter._id = req.params.id || filter.id;
+          delete filter.id;
         }
-        else {
-          done(data);
-        }
+  
+        ds.schema.datasource.filter = {};
+  
+        _.each(filter, function(value, key) {
+            ds.schema.datasource.filter[key] = value;
+        });
+  
+        // rebuild datasource endpoint with filters
+        var d = new Datasource();
+        d.buildEndpoint(ds.schema, function(endpoint) {
+          ds.endpoint = endpoint;
+        });
+  
       }
+  
+      help.getData(ds, function(result) {
+  
+        if (result) {
+          data[key] = (typeof result === 'object' ? result : JSON.parse(result));
+        }
+  
+        idx++;
+  
+        // if we're at the end of the datasources array, 
+        // start processing the attached events
+        if (idx === Object.keys(self.datasources).length) {
+  
+  
+          if (0 !== Object.keys(self.events).length) {
+            var eventIdx = 0;
+            _.each(self.events, function(value, key) {
+              
+                self.events[key].run(req, res, function(result) {
+                  data[key] = result;
+                });
+  
+                eventIdx++;
+  
+                // return the data if we're at the end of the events
+                // array, we have all the responses to render the page
+                if (eventIdx === Object.keys(self.events).length) {
+                  done(data);
+                }
+            });
+          }
+          else {
+            done(data);
+          }
+        }
+      });
     });
-  });
+  } else {
+    done(data);
+  }
 };
 
 module.exports = function (page, options) {
