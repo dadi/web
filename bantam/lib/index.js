7dda195dd61c21d784b08f831d1872d399c0d916
James Lambie <jameslambie@gmail.com>
2015-09-11 23:56:08 +0800

    slack messaging

--- bantam/lib/index.js
+++ bantam/lib/index.js
@@ -76,6 +76,32 @@ Server.prototype.start = function (options, done) {
     server.on('listening', function (e) {
       console.log('\nStarted Rosecomb on ' + config.server.host + ':' + config.server.port + "\n");
       logger.prod('Started Rosecomb on ' + config.server.host + ':' + config.server.port);
+
+      var Slack = require('node-slack');
+      var slack = new Slack('https://hooks.slack.com/services/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', {});
+      
+      slack.send({
+        text: 'Started Rosecomb on ' + config.server.host + ':' + config.server.port,
+        username: 'Bantam',
+        icon_emoji: ':bantam:',
+        "attachments": [
+            {
+                "fallback": "Required text summary of the attachment that is shown by clients that understand attachments but choose not to show them.",
+                "text": "Optional text that should appear within the attachment",
+                "pretext": "Optional text that should appear above the formatted data",
+                "color": "good", // Can either be one of 'good', 'warning', 'danger', or any hex color code
+                // Fields are displayed in a table on the message
+                "fields": [
+                    {
+                        "title": "Required Field Title", // The title may not contain markup and will be escaped for you
+                        "value": "Text value of the field. May contain standard message markup and must be escaped as normal. May be multi-line.",
+                        "short": false // Optional flag indicating whether the `value` is short enough to be displayed side-by-side with other values
+                    }
+                ]
+            }
+        ]
+      });
+
     });
 
     server.on('error', function (e) {
