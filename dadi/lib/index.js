8b06c2f2b88ee708ce9b79df99f0e5444395eafa
James Lambie <jameslambie@gmail.com>
2016-03-16 12:38:13 +0800

    include dadi status and endpoint

--- dadi/lib/index.js
+++ dadi/lib/index.js
@@ -1,5 +1,6 @@
 
 var version = require('../../package.json').version;
+var site = require('../../package.json').name;
 var nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
 
 var _ = require('underscore');
@@ -19,6 +20,7 @@ var serveStatic = require('serve-static');
 var session = require('express-session');
 var toobusy = require('toobusy-js');
 var url = require('url');
+var dadiStatus = require('@dadi/status');
 
 var mongoStore;
 if (nodeVersion < 1) {
@@ -159,6 +161,33 @@ Server.prototype.start = function (done) {
         next();
       }
     });
+
+    app.use('/api/status', function(req, res, next) {
+      var params = {
+        site: site,
+        package: '@dadi/web',
+        version: version,
+        healthCheck: {
+          baseUrl: 'http://127.0.0.1:3001',
+          authorization: 'Bearer 123abcdef',
+          routes: [{
+            route: '/',
+            expectedResponseTime: 10
+          }]
+        }
+      }
+
+      dadiStatus(params, function(err, data) {
+        if (err) return next(err);
+        var resBody = JSON.stringify(data, null, 2);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('content-length', Buffer.byteLength(resBody));
+        res.end(resBody);
+      });
+    });
+
 
     if (config.get('api.enabled')) {
       // caching layer
@@ -634,7 +663,7 @@ Server.prototype.dustCompile = function (options) {
 
       fs.readFile(template, { encoding: 'utf8' }, function (err, data) {
         if (err) {
-          // no template file found? 
+          // no template file found?
           return callback(err, null);
         }
 
