74e53eaf9cc53d478b31121ca78f239ee93034d3
Eduardo Boucas <eb@dadi.co>
2016-03-25 14:32:56 +0000

    Add routine to create token wallet directory

--- dadi/lib/auth/index.js
+++ dadi/lib/auth/index.js
@@ -4,6 +4,8 @@
 var config = require(__dirname + '/../../../config.js');
 var help = require(__dirname + '/../help');
 var log = require(__dirname + '/../log');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
 var Passport = require(__dirname + '/../../../../passport/node/src'); // !!! TODO: Replace with NPM
 
@@ -14,7 +16,7 @@ module.exports.getToken = function () {
     issuer: {
       uri: config.get('api.protocol') + '://' + config.get('api.host'),
       port: config.get('api.port'),
-      endpoint: config.get('auth.tokenUrl')  
+      endpoint: config.get('auth.tokenUrl')
     },
     credentials: {
       clientId: config.get('auth.clientId'),
@@ -29,25 +31,35 @@ module.exports.getToken = function () {
 
 // This attaches middleware to the passed in app instance
 module.exports = function (server) {
-  server.app.use(function (req, res, next) {
-    log.info({module: 'auth'}, 'Retrieving access token for "' + req.url + '"');
-    help.timer.start('auth');
+  mkdirp(path.resolve(config.get('paths.tokenWallet')), {}, function (err, made) {
+    if (err) {
+      console.log(err);
+    }
 
-    self.getToken().then(function (bearerToken) {
-      help.timer.stop('auth');
+    if (made) {
+      console.log('Token wallet directory created at ' + made);
+    }
 
-      return next();
-    }).catch(function (errorData) {
-      var err = new Error();
-      err.name = errorData.title;
-      err.message = errorData.detail;
-      err.remoteIp = options.issuer.uri;
-      err.remotePort = options.issuer.port;
-      err.path = options.issuer.endpoint;
+    server.app.use(function (req, res, next) {
+      log.info({module: 'auth'}, 'Retrieving access token for "' + req.url + '"');
+      help.timer.start('auth');
 
-      help.timer.stop('auth');
+      self.getToken().then(function (bearerToken) {
+        help.timer.stop('auth');
 
-      return next(err);
+        return next();
+      }).catch(function (errorData) {
+        var err = new Error();
+        err.name = errorData.title;
+        err.message = errorData.detail;
+        err.remoteIp = config.get('api.host');
+        err.remotePort = config.get('api.port');
+        err.path = config.get('auth.tokenUrl');
+
+        help.timer.stop('auth');
+
+        return next(err);
+      });    
     });    
   });
 };
