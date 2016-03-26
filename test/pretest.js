46a397df903829913d819330d55e4286b01a6063
Eduardo Boucas <eb@dadi.co>
2016-03-26 00:50:13 +0000

    Fix issue where test breaks without sample config

--- test/pretest.js
+++ test/pretest.js
@@ -5,14 +5,14 @@ var colors = require('colors');
 var testConfigPath = './config/config.test.json';
 var testConfigSamplePath = './config/config.test.json.sample';
 
-var testConfigSample = fs.readFileSync(testConfigSamplePath, { encoding: 'utf-8'});
-
 function loadConfig() {
   try {
     var testConfig = fs.readFileSync(testConfigPath, { encoding: 'utf-8'});
   }
   catch (err) {
     if (err.code === 'ENOENT') {
+      var testConfigSample = fs.readFileSync(testConfigSamplePath, { encoding: 'utf-8'});
+
       fs.writeFileSync(testConfigPath, testConfigSample);
       loadConfig();
     }
