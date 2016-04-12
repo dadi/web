938dbfd97a9193feb023b9ef0676905e3bdfd0b8
James Lambie <jameslambie@gmail.com>
2016-04-12 08:56:34 +0800

    fix: use path.resolve to locate cache folder

--- test/help.js
+++ test/help.js
@@ -210,7 +210,7 @@ module.exports.clearCache = function () {
 
     // for each directory in the cache folder, remove all files then
     // delete the folder
-    fs.readdirSync(config.get('caching.directory.path')).forEach(function (dirname) {
-        deleteFolderRecursive(path.join(config.get('caching.directory.path'), dirname));
+    fs.readdirSync(path.resolve(config.get('caching.directory.path'))).forEach(function (dirname) {
+        deleteFolderRecursive(path.join(path.resolve(config.get('caching.directory.path')), dirname));
     });
 }
