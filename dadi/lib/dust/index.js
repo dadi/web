e8a3f88ea90d3d53395f645adc5f876835370628
Eduardo Boucas <eb@dadi.co>
2016-04-17 11:35:20 +0000

    feat: add the ability to export Dust filters to client

--- dadi/lib/dust/index.js
+++ dadi/lib/dust/index.js
@@ -7,6 +7,9 @@ var path = require('path');
 var Dust = function () {
   this.templates = {};
 
+  // Loading core Dust helpers
+  require('dustjs-helpers');
+
 	dust.onLoad = (function (templateName, opts, callback) {
     var template = templateName + '.dust';
 
@@ -27,17 +30,124 @@ var Dust = function () {
   }).bind(this);
 };
 
-Dust.prototype._buildHelpersFile = function () {
-  var helpersFile = '';
+Dust.prototype._exportFunctionsAs = function (type) {
+  var output = '';
 
-  Object.keys(dust.helpers).forEach(function (helper) {
-    helpersFile += ('dust.helpers.' + helper + '=' + dust.helpers[helper].toString() + ';');
+  Object.keys(dust[type]).forEach(function (item) {
+    var itemCode = dust[type][item].toString();
+
+    if (itemCode.indexOf('[native code]') !== -1) {
+      return;
+    }
+
+    output += ('dust.' + type +  '.' + item + '=' + itemCode + ';');
   });
 
-  return helpersFile;
+  return output;
 };
 
-Dust.prototype._requireDirectory = function (directory) {
+Dust.prototype._writeToFile = function (filePath, content, append) {
+  return new Promise(function (resolve, reject) {
+    mkdirp(path.dirname(filePath), function (err, made) {
+      if (err) {
+        log.error({module: 'dust'}, {err: err}, "Error creating directory for file '%s'", filePath);
+
+        return reject(err);
+      }
+
+      var writeFunction = append ? fs.appendFile : fs.writeFile;
+
+      writeFunction.call(this, filePath, content, function (err) {
+        if (err) {
+          log.error({module: 'dust'}, {err: err}, "Error writing to file '%s'", filePath);
+
+          return reject(err);
+        }
+
+        resolve(content);
+      });
+    });
+  });
+};
+
+Dust.prototype.clearCache = function () {
+  dust.cache = {};
+};
+
+Dust.prototype.compile = function (source, templateName, load) {
+  load = (load !== false);
+
+  // Skipping compilation if template is already loaded
+  if (this.isLoaded(templateName)) return;
+
+  try {
+    var compiled = dust.compile(source, templateName);
+
+    if (load) {
+      dust.loadSource(compiled);
+    }
+
+    this.templates[templateName] = compiled;
+  } catch (err) {
+    log.error({module: 'dust'}, {err: err}, "Couldn\'t compile Dust template '%s'", templateName);
+  }
+
+  return compiled;
+};
+
+Dust.prototype.compileDirectory = function (directory, prefix, recursive) {
+  prefix = prefix || '';
+
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    fs.readdir(directory, function (err, files) {
+      var filesAbsolute = files.map(function (file) {
+        return path.join(directory, file);
+      });
+
+      resolve(self.compileFiles(filesAbsolute, prefix, recursive));
+    });
+  });
+};
+
+Dust.prototype.compileFiles = function (files, prefix, recursive) {
+  prefix = prefix || '';
+
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    var queue = [];
+
+    files.forEach(function (file) {
+      fs.stat(file, function (err, stats) {
+        var basename = path.basename(file, '.dust');
+
+        if (stats.isDirectory() && recursive) {
+          queue.push(self.compileDirectory(file, path.join(prefix, basename)));
+        } else if (stats.isFile() && (path.extname(file) === '.dust')) {
+          var name = path.join(prefix, basename);
+
+          fs.readFile(file, 'utf8', function (err, data) {
+            queue.push(Promise.resolve(self.compile(data, name)));
+          });
+        }
+      });
+    });
+
+    resolve(Promise.all(queue));
+  });
+};
+
+Dust.prototype.getEngine = function () {
+  return dust;
+};
+
+Dust.prototype.isLoaded = function (templateName) {
+  return dust.cache.hasOwnProperty(templateName);
+};
+
+Dust.prototype.loadDirectory = function (directory) {
   return new Promise(function (resolve, reject) {
     fs.stat(directory, function (err, stats) {
       if (err) {
@@ -81,43 +191,6 @@ Dust.prototype._requireDirectory = function (directory) {
   });
 };
 
-Dust.prototype.clearCache = function () {
-  dust.cache = {};
-};
-
-Dust.prototype.compile = function (source, templateName, load) {
-  load = (load !== false);
-
-  var compiled = dust.compile(source, templateName);
-
-  if (load) {
-    dust.loadSource(compiled);
-  }
-
-  this.templates[templateName] = compiled;
-
-  return compiled;
-};
-
-Dust.prototype.getEngine = function () {
-  return dust;
-};
-
-Dust.prototype.isLoaded = function (templateName) {
-  return dust.cache.hasOwnProperty(templateName);
-};
-
-Dust.prototype.loadFilters = function () {
-  return this._requireDirectory(this.options.filtersPath);
-};
-
-Dust.prototype.loadHelpers = function () {
-  // Loading core Dust helpers
-  require('dustjs-helpers');
-
-  return this._requireDirectory(this.options.helpersPath);
-};
-
 Dust.prototype.render = function (templateName, data, callback) {
   dust.render(templateName, data, callback);
 };
@@ -138,71 +211,57 @@ Dust.prototype.setOptions = function (options) {
   this.options = options;
 };
 
-Dust.prototype.writeClientsideTemplates = function () {
+Dust.prototype.writeClientsideFiles = function () {
+  var self = this;
   var compiledTemplates = this.templates;
 
   if (config.get('dust.clientRender.enabled')) {
-    if (config.get('dust.clientRender.helpersOutputPath') !== '') {
-      var helpersOutputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.helpersOutputPath'));
-      var helpersOutput = this._buildHelpersFile();
-      
-      mkdirp(path.dirname(helpersOutputFile), function (err, made) {
-        if (err) {
-          log.error({module: 'dust'}, {err: err}, 'Error creating directory for helpers');
+    var templatesQueue = [];
 
-          return;
-        }
-
-        fs.writeFile(helpersOutputFile, helpersOutput, function (err) {
-          if (err) {
-            log.error({module: 'dust'}, {err: err}, "Error writing helpers to file '%s'", helpersOutputFile);
-
-            return;
-          }
-        });
-      });
-    }
-
-    if (config.get('dust.clientRender.outputFormat') === 'combined') {
-      var outputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.outputPath'));
+    // Step 1. Write templates
+    if (config.get('dust.clientRender.templates.format') === 'combined') {
+      var templatesOutputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.templates.path'));
       var templatesOutput = '';
 
       Object.keys(compiledTemplates).forEach(function (name) {
         templatesOutput += compiledTemplates[name];
       });
 
-      mkdirp(path.dirname(outputFile), {}, function (err, made) {
-        if (err) {
-          log.error({module: 'dust'}, {err: err}, 'Error creating directory for compiled template');
-
-          return;
-        }
-
-        fs.writeFile(outputFile, templatesOutput, function (err) {
-          if (err) {
-            log.error({module: 'dust'}, {err: err}, "Error writing compiled template to file '%s'", outputFile);
-          }
-        });
-      });
+      templatesQueue.push(this._writeToFile(templatesOutputFile, templatesOutput));
     } else {
-      Object.keys(compiledTemplates).forEach(function (name) {
-        var outputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.outputPath'), name) + '.js';
+      Object.keys(compiledTemplates).forEach((function (name) {
+        var templatesOutputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.templates.path'), name) + '.js';
 
-        mkdirp(path.dirname(outputFile), {}, function (err, made) {
-          if (err) {
-            log.error({module: 'dust'}, {err: err}, 'Error creating directory for compiled template');
-
-            return;
-          }
-
-          fs.writeFile(outputFile, compiledTemplates[name], function (err) {
-            if (err) {
-              log.error({module: 'dust'}, {err: err}, "Error writing compiled template to file '%s'", outputFile);
-            }
-          });
-        });
-      });
+        templatesQueue.push(this._writeToFile(templatesOutputFile, compiledTemplates[name]));
+      }).bind(this));
     }
+
+    return Promise.all(templatesQueue)
+                  .then(function (templates) {
+                    // Step 2. Write filters
+                    if (config.get('dust.clientRender.filters.path') !== '') {
+                      var filtersOutputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.filters.path'));
+                      var filtersOutput = self._exportFunctionsAs('filters');
+                      var fileAppend = (config.get('dust.clientRender.filters.path') === config.get('dust.clientRender.templates.path'));
+
+                      return self._writeToFile(filtersOutputFile, filtersOutput, fileAppend);
+                    }
+
+                    return Promise.resolve(true);
+                  })
+                  .then(function (filters) {
+                    // Step 3. Write helpers
+                    if (config.get('dust.clientRender.helpers.path') !== '') {
+                      var helpersOutputFile = path.join(config.get('paths.public'), config.get('dust.clientRender.helpers.path'));
+                      var helpersOutput = self._exportFunctionsAs('helpers');
+                      var fileAppend = ((config.get('dust.clientRender.helpers.path') === config.get('dust.clientRender.templates.path')) ||
+                                        (config.get('dust.clientRender.helpers.path') === config.get('dust.clientRender.filters.path')));
+
+                      return self._writeToFile(helpersOutputFile, helpersOutput, fileAppend);
+                    }
+
+                    return Promise.resolve(true);
+                  });
   }
 };
 
