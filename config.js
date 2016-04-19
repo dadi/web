175b5b7664b96fe29ccefa48bf562d2acedf9806
Eduardo Boucas <eb@dadi.co>
2016-04-19 21:39:03 +0000

    refactor: change clientRender config options

--- config.js
+++ config.js
@@ -162,12 +162,12 @@ var conf = convict({
       default: false
     },
     clientRender: {
-      enabled: {
-        doc: "If true, compiled templates are made available to the client-side",
-        format: Boolean,
-        default: false
-      },
       filters: {
+        enabled: {
+          doc: "If true, filters are made available to the client-side",
+          format: Boolean,
+          default: false
+        },
         path: {
           doc: "The location where filters should be written to. If empty, filters will not be included.",
           format: String,
@@ -175,6 +175,11 @@ var conf = convict({
         }
       },
       helpers: {
+        enabled: {
+          doc: "If true, helpers are made available to the client-side",
+          format: Boolean,
+          default: false
+        },
         path: {
           doc: "The location where helpers should be written to. If empty, helpers will not be included.",
           format: String,
@@ -182,13 +187,18 @@ var conf = convict({
         }
       },
       templates: {
+        enabled: {
+          doc: "If true, compiled templates are made available to the client-side",
+          format: Boolean,
+          default: false
+        },
         format: {
           doc: "Defines whether compiled templates are written to individual JS files ('separate') or combined into a single one ('combined')",
           format: ["separate", "combined"],
           default: "separate"
         },
         path: {
-          doc: "The location where compiled templates should be written to, relative to 'public'. This should be a folder when 'outputFormat' is 'separate' and a file when 'combined'",
+          doc: "The location where compiled templates should be written to, relative to 'public'. This should be a folder when 'format' is 'separate' and a file when 'combined'",
           format: String,
           default: "templates"
         }
