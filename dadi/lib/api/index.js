7678459b7ab59dbd1d46dd76865cc1dcc3b9117a
James Lambie <jameslambie@gmail.com>
2016-08-01 21:48:53 +0800

    style: reformat to standardjs

--- dadi/lib/api/index.js
+++ dadi/lib/api/index.js
@@ -14,57 +14,56 @@ var config = require(__dirname + '/../../../config')
  * @constructor
  */
 var Api = function () {
-    this.paths = []
-    this.all = []
-    this.errors = []
+  this.paths = []
+  this.all = []
+  this.errors = []
 
-    // Sentry error handler
-    if (config.get('logging.sentry.dsn') !== "") {
-      this.errors.push(raven.middleware.express.errorHandler(config.get('logging.sentry.dsn')))
+  // Sentry error handler
+  if (config.get('logging.sentry.dsn') !== '') {
+    this.errors.push(raven.middleware.express.errorHandler(config.get('logging.sentry.dsn')))
+  }
+
+  // Fallthrough error handler
+  this.errors.push(onError(this))
+
+  // permanently bind context to listener
+  this.listener = this.listener.bind(this)
+
+  this.protocol = config.get('server.protocol') || 'http'
+
+  if (this.protocol === 'https') {
+    var readFileSyncSafe = (path) => {
+      try { return fs.readFileSync(path) } catch (ex) {}
+      return null
     }
 
-    // Fallthrough error handler
-    this.errors.push(onError(this))
-
-    // permanently bind context to listener
-    this.listener = this.listener.bind(this)
-
-    this.protocol = config.get('server.protocol') || 'http'
-
-    if (this.protocol === 'https') {
-        var readFileSyncSafe = (path) => {
-            try { return fs.readFileSync(path) }
-            catch (ex) {}
-            return null
-        }
-
-        var passphrase = config.get('server.sslPassphrase')
-        var caPath = config.get('server.sslIntermediateCertificatePath')
-        var caPaths = config.get('server.sslIntermediateCertificatePaths')
-        var serverOptions = {
-            key: readFileSyncSafe(config.get('server.sslPrivateKeyPath')),
-            cert: readFileSyncSafe(config.get('server.sslCertificatePath'))
-        }
-
-        if (passphrase && passphrase.length >= 4) {
-            serverOptions.passphrase = passphrase
-        }
-
-        if (caPaths && caPaths.length > 0) {
-            serverOptions.ca = []
-            caPaths.forEach((path) => {
-                var data = readFileSyncSafe(path)
-                data && serverOptions.ca.push(data)
-            })
-        } else if (caPath && caPath.length > 0) {
-            serverOptions.ca = readFileSyncSafe(caPath)
-        }
-
-        this.serverInstance = https.createServer(serverOptions, this.listener)
-    } else {
-        // default to http
-        this.serverInstance = http.createServer(this.listener)
+    var passphrase = config.get('server.sslPassphrase')
+    var caPath = config.get('server.sslIntermediateCertificatePath')
+    var caPaths = config.get('server.sslIntermediateCertificatePaths')
+    var serverOptions = {
+      key: readFileSyncSafe(config.get('server.sslPrivateKeyPath')),
+      cert: readFileSyncSafe(config.get('server.sslCertificatePath'))
     }
+
+    if (passphrase && passphrase.length >= 4) {
+      serverOptions.passphrase = passphrase
+    }
+
+    if (caPaths && caPaths.length > 0) {
+      serverOptions.ca = []
+      caPaths.forEach((path) => {
+        var data = readFileSyncSafe(path)
+        data && serverOptions.ca.push(data)
+      })
+    } else if (caPath && caPath.length > 0) {
+      serverOptions.ca = readFileSyncSafe(caPath)
+    }
+
+    this.serverInstance = https.createServer(serverOptions, this.listener)
+  } else {
+    // default to http
+    this.serverInstance = http.createServer(this.listener)
+  }
 }
 
 /**
@@ -75,24 +74,23 @@ var Api = function () {
  *  @api public
  */
 Api.prototype.use = function (path, handler) {
+  if (typeof path === 'function') {
+    if (path.length === 4) return this.errors.push(path)
+    return this.all.push(path)
+  }
 
-    if (typeof path === 'function') {
-        if (path.length === 4) return this.errors.push(path)
-        return this.all.push(path)
-    }
+  var regex = pathToRegexp(path)
 
-    var regex = pathToRegexp(path)
+  this.paths.push({
+    path: path,
+    order: routePriority(path, regex.keys),
+    handler: handler,
+    regex: regex
+  })
 
-    this.paths.push({
-        path: path,
-        order: routePriority(path, regex.keys),
-        handler: handler,
-        regex: regex
-    })
-
-    this.paths.sort((a,b) => {
-        return b.order - a.order
-    })
+  this.paths.sort((a, b) => {
+    return b.order - a.order
+  })
 }
 
 /**
@@ -102,28 +100,27 @@ Api.prototype.use = function (path, handler) {
  *  @api public
  */
 Api.prototype.unuse = function (path) {
-    var indx = 0
-    if (typeof path === 'function') {
-        if (path.length === 4) {
-            indx = this.errors.indexOf(path)
-            return !!~indx && this.errors.splice(indx, 1)
-        }
-
-        var functionStr = path.toString()
-        _.each(this.all, function (func) {
-            if (func.toString() === functionStr) {
-                return this.all.splice(indx, 1)
-            }
-            else {
-                indx++
-            }
-        }, this)
-
-        // indx = this.all.indexOf(path)
-        // return !!~indx && this.all.splice(indx, 1)
+  var indx = 0
+  if (typeof path === 'function') {
+    if (path.length === 4) {
+      indx = this.errors.indexOf(path)
+      return !!~indx && this.errors.splice(indx, 1)
     }
-    var existing = _.findWhere(this.paths, { path: path })
-    this.paths = _.without(this.paths, existing)
+
+    var functionStr = path.toString()
+    _.each(this.all, function (func) {
+      if (func.toString() === functionStr) {
+        return this.all.splice(indx, 1)
+      } else {
+        indx++
+      }
+    }, this)
+
+  // indx = this.all.indexOf(path)
+  // return !!~indx && this.all.splice(indx, 1)
+  }
+  var existing = _.findWhere(this.paths, { path: path })
+  this.paths = _.without(this.paths, existing)
 }
 
 /**
@@ -136,7 +133,7 @@ Api.prototype.unuse = function (path) {
  *  @api public
  */
 Api.prototype.listen = function (port, host, backlog, done) {
-    return this.serverInstance.listen(port, host, backlog, done)
+  return this.serverInstance.listen(port, host, backlog, done)
 }
 
 /**
@@ -148,50 +145,48 @@ Api.prototype.listen = function (port, host, backlog, done) {
  */
 Api.prototype.listener = function (req, res) {
 
-    // clone the middleware stack
-    var stack = this.all.slice(0)
-    var path = url.parse(req.url).pathname
+  // clone the middleware stack
+  var stack = this.all.slice(0)
+  var path = url.parse(req.url).pathname
 
-    req.paths = []
+  req.paths = []
 
-    // get matching routes, and add req.params
-    var matches = this._match(path, req)
+  // get matching routes, and add req.params
+  var matches = this._match(path, req)
 
-    var originalReqParams = req.params
+  var originalReqParams = req.params
 
-    var doStack = function (i) {
-        return function (err) {
+  var doStack = function (i) {
+    return function (err) {
+      if (err) return errStack(0)(err)
 
-            if (err) return errStack(0)(err)
+      // add the original params back, in case a middleware
+      // has modified the current req.params
+      _.extend(req.params, originalReqParams)
 
-            // add the original params back, in case a middleware
-            // has modified the current req.params
-            _.extend(req.params, originalReqParams)
-
-            try {
-              stack[i](req, res, doStack(++i))
-            }
-            catch (e) {
-              return errStack(0)(e)
-            }
-        }
+      try {
+        stack[i](req, res, doStack(++i))
+      } catch (e) {
+        return errStack(0)(e)
+      }
     }
+  }
 
-    var self = this
-    var errStack = function (i) {
-        return function (err) {
-            self.errors[i](err, req, res, errStack(++i))
-        }
+  var self = this
+  var errStack = function (i) {
+    return function (err) {
+      self.errors[i](err, req, res, errStack(++i))
     }
+  }
 
-    // add path specific handlers
-    stack = stack.concat(matches)
+  // add path specific handlers
+  stack = stack.concat(matches)
 
-    // add 404 handler
-    stack.push(notFound(this, req, res))
+  // add 404 handler
+  stack.push(notFound(this, req, res))
 
-    // start going through the middleware/routes
-    doStack(0)()
+  // start going through the middleware/routes
+  doStack(0)()
 }
 
 /**
@@ -202,43 +197,42 @@ Api.prototype.listener = function (req, res) {
  *  @api private
  */
 Api.prototype._match = function (path, req) {
-    var paths = this.paths
-    var matches = []
-    var handlers = []
+  var paths = this.paths
+  var matches = []
+  var handlers = []
 
-    // always add params object to avoid need for checking later
-    req.params = {}
+  // always add params object to avoid need for checking later
+  req.params = {}
 
-    for (i = 0; i < paths.length; i++) {
-        var match = paths[i].regex.exec(path)
+  for (i = 0; i < paths.length; i++) {
+    var match = paths[i].regex.exec(path)
 
-        if (!match) { continue }
+    if (!match) { continue }
 
-        req.paths.push(paths[i].path)
+    req.paths.push(paths[i].path)
 
-        var keys = paths[i].regex.keys
-        handlers.push(paths[i].handler)
+    var keys = paths[i].regex.keys
+    handlers.push(paths[i].handler)
 
-        match.forEach(function (k, i) {
-            var keyOpts = keys[i] || {}
-            if (match[i + 1] && keyOpts.name && !req.params[keyOpts.name]) req.params[keyOpts.name] = match[i + 1]
-        })
+    match.forEach(function (k, i) {
+      var keyOpts = keys[i] || {}
+      if (match[i + 1] && keyOpts.name && !req.params[keyOpts.name]) req.params[keyOpts.name] = match[i + 1]
+    })
 
-        //break
-    }
+  // break
+  }
 
-    return handlers
+  return handlers
 }
 
 module.exports = function () {
-    return new Api()
+  return new Api()
 }
 
 module.exports.Api = Api
 
-function onError(api) {
+function onError (api) {
   return function (err, req, res, next) {
-
     if (res.finished) return
 
     if (config.get('env') === 'development') {
@@ -252,7 +246,7 @@ function onError(api) {
       statusCode: err.statusCode || 500,
       code: err.name,
       message: err.message,
-      stack : err.stack.split('\n')
+      stack: err.stack.split('\n')
     }
 
     // look for a loaded path that matches the error code
@@ -264,8 +258,7 @@ function onError(api) {
       req.error = data
       res.statusCode = data.statusCode
       path.handler(req, res)
-    }
-    else {
+    } else {
       // no page found to display the error, output raw data
       res.statusCode = 500
       res.setHeader('Content-Type', 'application/json')
@@ -275,48 +268,46 @@ function onError(api) {
 }
 
 // return a 404
-function notFound(api, req, res) {
-    return function () {
+function notFound (api, req, res) {
+  return function () {
+    res.statusCode = 404
 
-        res.statusCode = 404
+    // look for a 404 page that has been loaded
+    // along with the rest of the API, and call its
+    // handler if it exists
 
-        // look for a 404 page that has been loaded
-        // along with the rest of the API, and call its
-        // handler if it exists
-
-        var path = _.findWhere(api.paths, { path: '/404' })
-        if (path) {
-            path.handler(req, res)
-        }
-        // otherwise, respond with default message
-        else {
-            res.end("HTTP 404 Not Found")
-        }
+    var path = _.findWhere(api.paths, { path: '/404' })
+    if (path) {
+      path.handler(req, res)
     }
+    // otherwise, respond with default message
+    else {
+      res.end('HTTP 404 Not Found')
+    }
+  }
 }
 
-function routePriority(path, keys) {
+function routePriority (path, keys) {
+  var tokens = pathToRegexp.parse(path)
 
-    var tokens = pathToRegexp.parse(path)
+  var staticRouteLength = 0
+  if (typeof tokens[0] === 'string') {
+    staticRouteLength = _.compact(tokens[0].split('/')).length
+  }
 
-    var staticRouteLength = 0
-    if (typeof tokens[0] === 'string') {
-        staticRouteLength = _.compact(tokens[0].split('/')).length
-    }
+  var requiredParamLength = _.filter(keys, function (key) {
+    return !key.optional
+  }).length
 
-    var requiredParamLength = _.filter(keys, function (key) {
-        return !key.optional
-    }).length
+  var optionalParamLength = _.filter(keys, function (key) {
+    return key.optional
+  }).length
 
-    var optionalParamLength = _.filter(keys, function (key) {
-        return key.optional
-    }).length
+  var order = (staticRouteLength * 5) + (requiredParamLength * 2) + (optionalParamLength)
 
-    var order = (staticRouteLength * 5) + (requiredParamLength * 2) + (optionalParamLength)
+  // make internal routes less important...
+  if (path.indexOf('/config') > 0) order = -100
+  if (path.indexOf('/api/') > 0) order = -100
 
-    // make internal routes less important...
-    if (path.indexOf('/config') > 0) order = -100
-    if (path.indexOf('/api/') > 0) order = -100
-
-    return order
+  return order
 }
