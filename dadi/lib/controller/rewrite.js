6104bf756c37ee999b867480a1f108200f37e657
James Lambie <jameslambie@gmail.com>
2016-11-14 12:56:29 +0000

    feat: add cache control headers to redirects

--- dadi/lib/controller/rewrite.js
+++ dadi/lib/controller/rewrite.js
@@ -6,6 +6,8 @@ var qs = require('qs')
 var httpReq = require('http').request
 var httpsReq = require('https').request
 var defaultVia = '1.1 ' + require('os').hostname()
+var path = require('path')
+var config = require(path.resolve(path.join(__dirname, '/../../../config')))
 
 /**
  * Syntaxes
@@ -96,10 +98,19 @@ module.exports = function (rules) {
 
       // Redirect
       if (rule.redirect) {
-        res.writeHead(rule.redirect, {
+        var headers = {
           Location: location
+        }
+
+        _.each(config.get('headers.cacheControl'), (value, key) => {
+          if (rule.redirect.toString() === key && value !== '') {
+            headers['Cache-Control'] = value
+          }
         })
+
+        res.writeHead(rule.redirect, headers)
         res.end()
+
         callNext = false
         return true
       }
