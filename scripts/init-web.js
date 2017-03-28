49fd1fd74504c12ad54c8166b1260cf05faf7ad0
David Longworth <dave@abovebored.co.uk>
2017-03-28 17:52:42 +0100

    chore: update init script

--- scripts/init-web.js
+++ scripts/init-web.js
@@ -4,13 +4,13 @@ var fs = require('fs')
 var path = require('path')
 
 var currentPath = process.cwd() // should be node_modules/@dadi/web
-var destinationFile = path.join(currentPath, '../../../index.js')
+var destinationFile = path.join(currentPath, '../../../server.js')
 
 // Add an server.js (which runs on npm start) file containing - require('@dadi/web')
 // More info: https://docs.npmjs.com/cli/start
 if (!fs.existsSync(destinationFile)) {
   fs.writeFile(destinationFile, "require('@dadi/web')", function(err) {
       if (err) return console.log(err)
-      console.log("index.js was created!")
+      console.log("server.js was created!")
   }); 
 }
