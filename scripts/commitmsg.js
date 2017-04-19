633950c4a995c0ef4a0511049f10ee64436542a5
James Lambie <jameslambie@gmail.com>
2017-04-19 14:10:56 +1200

    chore: remove commitmsg script

--- scripts/commitmsg.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-console.log(process.env['GIT_PARAMS'])
-
-var validateMessage = function(message) {
-  console.log("commitmsg.js", message)
-  var isValid = true
-  return isValid
-}
