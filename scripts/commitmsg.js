c1ba52548748be682c4ed9fc34e9bc775cc68dfc
James Lambie <jameslambie@gmail.com>
2017-04-19 13:57:37 +1200

    style: add params to commitmsg

--- scripts/commitmsg.js
+++ scripts/commitmsg.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 
-var validateMessage = function (message) {
-  console.log('commitmsg.js', message)
+console.log(process.env['GIT_PARAMS'])
+
+var validateMessage = function(message) {
+  console.log("commitmsg.js", message)
   var isValid = true
   return isValid
 }
