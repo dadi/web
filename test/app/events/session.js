5df625bf10d8d58d967f5a40e1c6ced6d80d0890
James Lambie <jameslambie@gmail.com>
2017-06-27 12:41:21 +0100

    style: standardjs

--- test/app/events/session.js
+++ test/app/events/session.js
@@ -1,27 +1,20 @@
-var path = require('path');
-var http = require("http");
-var url = require('url');
-var querystring = require('querystring');
-var pathToRegexp = require('path-to-regexp');
-
 // the `data` parameter contains the data already loaded by
 // the page's datasources and any previous events that have fired
 var Event = function (req, res, data, callback) {
-
   if (req.session) {
-    req.session.active = true;
-    req.session.save(function(err) {
+    req.session.active = true
+    req.session.save(function (err) {
       // session saved
-    });
+    })
 
-    data.session_id = req.session.id;
+    data.session_id = req.session.id
   }
 
-  callback(null, data);
-};
+  callback(null, data)
+}
 
 module.exports = function (req, res, data, callback) {
-    return new Event(req, res, data, callback);
-};
+  return new Event(req, res, data, callback)
+}
 
-module.exports.Event = Event;
+module.exports.Event = Event
