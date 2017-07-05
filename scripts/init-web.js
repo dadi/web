399bc5699e28cd6cb8b425dcae3c347fc63938cb
Eduardo Bouças <mail@eduardoboucas.com>
2017-07-05 13:21:50 +0100

    fix: update post-install script to include reference to Dust engine

--- scripts/init-web.js
+++ scripts/init-web.js
@@ -14,7 +14,7 @@ if (~currentPath.indexOf('node_modules')) {
   fs.stat(destinationFile, (err, stats) => {
     if (err && err.code && err.code === 'ENOENT') {
       // file doesn't exist
-      fs.writeFile(destinationFile, "require('@dadi/web')", function(err) {
+      fs.writeFile(destinationFile, "require('@dadi/web')({engines:[require('@dadi/web-dustjs')]})", function(err) {
         if (err) return console.log(err)
         console.log('Web entry point created at', destinationFile)
       })
