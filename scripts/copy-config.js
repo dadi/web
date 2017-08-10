6e74b8b5b036f8eedbbf79c8f9fb94c58d3c55fe
David Longworth <dave@abovebored.co.uk>
2017-08-10 20:02:59 +0100

    style: comma

--- scripts/copy-config.js
+++ scripts/copy-config.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
 
-var fs = require("fs")
-var mkdirp = require("mkdirp")
-var path = require("path")
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var path = require('path')
 
 var currentPath = process.cwd() // should be node_modules/@dadi/web
 var configPath = path.join(
   __dirname,
-  "../config/config.development.json.sample"
+  '../config/config.development.json.sample'
 )
-var destinationDir = path.join(currentPath, "../../../config")
-var destinationFile = path.join(destinationDir, "config.development.json")
+var destinationDir = path.join(currentPath, '../../../config')
+var destinationFile = path.join(destinationDir, 'config.development.json')
 
 // Only run if in a node_modules folder
-if (~currentPath.indexOf("node_modules")) {
+if (~currentPath.indexOf('node_modules')) {
   mkdirp(destinationDir, (err, made) => {
     if (err) throw err
 
     fs.stat(destinationFile, (err, stats) => {
-      if (err && err.code && err.code === "ENOENT") {
+      if (err && err.code && err.code === 'ENOENT') {
         // file doesn't exist
         fs.readFile(configPath, (err, data) => {
           if (err) throw err
@@ -26,7 +26,7 @@ if (~currentPath.indexOf("node_modules")) {
           fs.writeFile(destinationFile, data, err => {
             if (err) throw err
 
-            console.log("Web configuration created at", destinationFile)
+            console.log('Web configuration created at', destinationFile)
           })
         })
       }
