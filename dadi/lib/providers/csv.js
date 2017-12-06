c713170f2fe8860e24a608de9a767523fe13f419
David Longworth <dave@abovebored.co.uk>
2017-12-06 13:07:24 +0000

    feat: csv provider

---
+++ dadi/lib/providers/csv.js
@@ -0,0 +1,219 @@
+'use strict'
+
+const async = require('async')
+const formatError = require('@dadi/format-error')
+const fs = require('fs')
+const path = require('path')
+const meta = require('@dadi/metadata')
+const recursive = require('recursive-readdir')
+const flatten = require('flat')
+
+const help = require(path.join(__dirname, '../help'))
+
+const CSVProvider = function () {}
+
+/**
+ * initialise - initialises the datasource provider
+ *
+ * @param  {obj} datasource - the datasource to which this provider belongs
+ * @param  {obj} schema - the schema that this provider works with
+ * @return {void}
+ */
+CSVProvider.prototype.initialise = function (datasource, schema) {
+  this.datasource = datasource
+  this.schema = schema
+}
+
+/**
+ * processSortParameter
+ *
+ * @param  {?} obj - sort parameter
+ * @return {?}
+ */
+CSVProvider.prototype.processSortParameter = function (obj) {
+  let sort = {}
+
+  if (
+    typeof obj !== 'undefined' &&
+    Object.keys(obj) &&
+    Object.keys(obj).length > 0
+  ) {
+    Object.keys(obj).forEach(field => {
+      const sortInteger = obj[field]
+
+      if (sortInteger === -1 || sortInteger === 1) {
+        sort[field] = sortInteger
+      }
+    })
+  }
+
+  return sort
+}
+
+/**
+ * load - loads data form the datasource
+ *
+ * @param  {string} requestUrl - url of the web request (not used)
+ * @param  {fn} done - callback on error or completion
+ * @return {void}
+ */
+CSVProvider.prototype.load = function (requestUrl, done) {
+  try {
+    const sourcePath = path.normalize(this.schema.datasource.source.path)
+
+    // Ignore files without the correct extension
+    recursive(sourcePath, (err, filepaths) => {
+      if (err && err.code === 'ENOENT') {
+        var data = {
+          results: [],
+          errors: [
+            formatError.createWebError('0006', { sourcePath: sourcePath })
+          ]
+        }
+
+        return done(null, data)
+      }
+
+      // Filter out only files with the correct extension
+      filepaths = filepaths.filter(i => new RegExp('.csv$', 'i').test(i))
+
+      // Process each file
+      async.map(filepaths, this.readFileAsync, (err, readResults) => {
+        if (err) return done(err, null)
+
+        this.parseRawDataAsync(readResults, posts => {
+          const params = this.datasourceParams
+            ? this.datasourceParams
+            : this.schema.datasource
+
+          const sort = this.processSortParameter(params.sort)
+          const search = params.search
+          const count = params.count
+          const fields = params.fields || []
+          const filters = params.filter
+          const page = params.page || 1
+
+          let metadata = []
+
+          // apply search
+          posts = help.where(posts, search)
+
+          // apply filter
+          posts = posts.filter(post => {
+            return Object.keys(filters).every(f => {
+              // Reduce dot notation to the value of given filter object key
+              let postRow = f.split('.').reduce((o, i) => o[i], post)
+              return postRow === filters[f]
+            })
+          })
+
+          // Sort posts by attributes field (with date support)
+          if (sort && Object.keys(sort).length > 0) {
+            Object.keys(sort).forEach(field => {
+              posts.sort(
+                help.sortBy(field, value => {
+                  if (field.toLowerCase().indexOf('date') > -1) {
+                    value = new Date(value)
+                  }
+
+                  return value
+                })
+              )
+
+              if (sort[field] === -1) {
+                posts.reverse()
+              }
+            })
+          }
+
+          // Count posts
+          let postCount = posts.length
+
+          // Paginate if required
+          if (page && count) {
+            const offset = (page - 1) * count
+            posts = posts.slice(offset, offset + count)
+
+            // Metadata for pagination
+            const options = []
+            options['page'] = parseInt(page)
+            options['limit'] = parseInt(count)
+
+            metadata = meta(options, parseInt(postCount))
+          }
+
+          if (fields && fields.length > 0) {
+            if (Array.isArray(posts)) {
+              let i = 0
+              posts.forEach(document => {
+                posts[i] = help.pick(posts[i], fields)
+                i++
+              })
+            } else {
+              posts = help.pick(posts, fields)
+            }
+          }
+
+          done(null, { results: posts, metadata: metadata || null })
+        })
+      })
+    })
+  } catch (ex) {
+    done(ex, null)
+  }
+}
+
+/**
+ * processRequest - called on every request, rebuild buildEndpoint
+ *
+ * @param  {obj} req - web request object
+ * @return {void}
+ */
+CSVProvider.prototype.processRequest = function (datasourceParams) {
+  this.datasourceParams = datasourceParams
+}
+
+CSVProvider.prototype.readFileAsync = function (filename, callback) {
+  fs.readFile(filename, 'utf8', function (err, data) {
+    return callback(err, { _name: filename, _contents: data })
+  })
+}
+
+CSVProvider.prototype.parseRawDataAsync = function (data, callback) {
+  const posts = []
+  const csvRegex = /^(.*?),"([.|\S|\s]*?)"$/gm
+
+  for (let i = 0; i < data.length; i++) {
+    // Some info about the file
+    const parsedPath = path.parse(data[i]._name)
+    const attributes = {
+      _id: parsedPath.name,
+      _ext: parsedPath.ext,
+      _loc: data[i]._name,
+      _path: parsedPath.dir
+        .replace(path.normalize(this.schema.datasource.source.path), '')
+        .replace(/^\/|\/$/g, '')
+        .split('/')
+    }
+
+    // Parse the csv data
+    let csv = csvRegex.exec(data[i]._contents)
+    let content = {}
+
+    // Loop over and create an obj from the csv
+    while (csv !== null) {
+      // the .replace decodes CSV encoded double quotes
+      content[csv[1]] = csv[2].replace(/""/gim, '"')
+      csv = csvRegex.exec(data[i]._contents)
+    }
+
+    posts.push({
+      attributes,
+      content: flatten.unflatten(content)
+    })
+  }
+
+  callback(posts)
+}
+
+module.exports = CSVProvider
