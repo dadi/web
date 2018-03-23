fe6bdecc51a31d28b48a1a3f9171569962e7fc61
David Longworth <david@longworth.to>
2018-03-23 18:56:27 +0000

    refactor: remove unused methods

--- dadi/lib/providers/remote.js
+++ dadi/lib/providers/remote.js
@@ -7,7 +7,6 @@ const https = require('https')
 const path = require('path')
 const zlib = require('zlib')
 
-const config = require(path.join(__dirname, '/../../../config.js'))
 const log = require('@dadi/logger')
 const DatasourceCache = require(path.join(__dirname, '/../cache/datasource'))
 
@@ -39,12 +38,11 @@ RemoteProvider.prototype.buildEndpoint = function (datasourceParams) {
     datasourceParams = this.schema.datasource
   }
 
-  const apiConfig = config.get('api')
   const source = datasourceParams.source || this.datasource.source
 
   const protocol = source.protocol || 'http'
-  const host = source.host || apiConfig.host
-  const port = source.port || apiConfig.port
+  const host = source.host
+  const port = source.port
 
   const uri = [
     protocol,
@@ -68,9 +66,9 @@ RemoteProvider.prototype.buildEndpoint = function (datasourceParams) {
  */
 RemoteProvider.prototype.load = function (requestUrl, done) {
   this.options = {
-    protocol: this.datasource.source.protocol || config.get('api.protocol'),
-    host: this.datasource.source.host || config.get('api.host'),
-    port: this.datasource.source.port || config.get('api.port'),
+    protocol: this.datasource.source.protocol || 'http',
+    host: this.datasource.source.host,
+    port: this.datasource.source.port || '80',
     path: url.parse(this.endpoint).path,
     // path: url.parse(requestUrl).path,
     method: 'GET'
@@ -372,28 +370,7 @@ RemoteProvider.prototype.getHeaders = function getHeaders (done) {
     'accept-encoding': 'gzip'
   }
 
-  // If the data-source has its own auth strategy, use it.
-  // Otherwise, authenticate with the main server via bearer token
-  if (this.authStrategy) {
-    // This could eventually become a switch statement that handles different auth types
-    if (this.authStrategy.getType() === 'bearer') {
-      this.authStrategy.getToken(
-        this.authStrategy,
-        false,
-        (err, bearerToken) => {
-          if (err) {
-            return done(err)
-          }
-
-          headers['Authorization'] = 'Bearer ' + bearerToken
-
-          return done(null, { headers: headers })
-        }
-      )
-    }
-  } else {
-    return done(null, { headers: headers })
-  }
+  return done(null, { headers: headers })
 }
 
 /**
