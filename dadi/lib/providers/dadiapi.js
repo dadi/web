ae8a0d09863aad2e82a07bdd19f07c8f8e8b63d2
David Longworth <david@longworth.to>
2018-08-28 15:53:39 +0100

    fix: make sure lang not undefined

--- dadi/lib/providers/dadiapi.js
+++ dadi/lib/providers/dadiapi.js
@@ -339,7 +339,7 @@ DadiApiProvider.prototype.processDatasourceParameters = function (
   const existingParams = qs.parse(url.parse(uri).search)
 
   const params = [
-    { lang: datasourceParams.lang },
+    { lang: datasourceParams.lang || '' },
     { count: datasourceParams.count || 0 },
     { skip: datasourceParams.skip },
     { page: datasourceParams.page || 1 },
